75d20a78de66ff920b13382e2f8287a8fadb65cc
Joseph Paz <jpaz@gruporpp.com.pe>
2018-12-20 18:17:33 -0500

    Se limipia la carpeta src

---
+++ public/precache-manifest.5c4f1feb674a312bd17019e787f4750a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "db6294aef5db02aec730",
+    "url": "/js/chunk-vendors.c2c96ab8.js"
+  },
+  {
+    "revision": "73e8dee57e05100a47db",
+    "url": "/js/app.193ea52e.js"
+  },
+  {
+    "revision": "c85c0b5a0252acf32754",
+    "url": "/js/about.e9a0cf3b.js"
+  },
+  {
+    "revision": "8fe2b07eb65add87d16ed9c9ee3aab21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "73e8dee57e05100a47db",
+    "url": "/css/app.736a4781.css"
+  }
+];
